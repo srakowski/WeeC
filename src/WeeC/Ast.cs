c0c9c0d0db4afe7445ad01279988ebf5137b6ae7
Shawn Rakowski <srakowski@live.com>
2018-08-25 10:26:10 -0500

    start on parser

---
+++ src/WeeC/Ast.cs
@@ -0,0 +1,22 @@
+﻿// MIT License - Copyright (C) Shawn Rakowski
+// This file is subject to the terms and conditions defined in
+// file 'LICENSE', which is part of this source code package.
+
+namespace WeeC
+{
+    internal class TranslationUnit
+    {
+    }
+
+    internal abstract class Declaration
+    {
+    }
+
+    internal abstract class Statement
+    {
+    }
+
+    internal abstract class Expression
+    {
+    }
+}
